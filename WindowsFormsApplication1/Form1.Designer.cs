901845ef887aee1e13460db96db1d177c34c7be4
Merhat <merhat1998@gmail.com>
2015-04-12 13:39:58 +0300

    Ping Pong Game
    The game added

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Game
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PlayGround = new System.Windows.Forms.Panel();
+            this.Pause = new System.Windows.Forms.PictureBox();
+            this.Continue = new System.Windows.Forms.PictureBox();
+            this.Exit = new System.Windows.Forms.PictureBox();
+            this.NewGame = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.Bot = new System.Windows.Forms.PictureBox();
+            this.Top = new System.Windows.Forms.PictureBox();
+            this.Player2 = new System.Windows.Forms.PictureBox();
+            this.Player1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.Bonus = new System.Windows.Forms.PictureBox();
+            this.PlayGround.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Pause)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Continue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Exit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NewGame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Top)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bonus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PlayGround
+            // 
+            this.PlayGround.Controls.Add(this.Bonus);
+            this.PlayGround.Controls.Add(this.Pause);
+            this.PlayGround.Controls.Add(this.Continue);
+            this.PlayGround.Controls.Add(this.Exit);
+            this.PlayGround.Controls.Add(this.NewGame);
+            this.PlayGround.Controls.Add(this.ball);
+            this.PlayGround.Controls.Add(this.Bot);
+            this.PlayGround.Controls.Add(this.Top);
+            this.PlayGround.Controls.Add(this.Player2);
+            this.PlayGround.Controls.Add(this.Player1);
+            this.PlayGround.Location = new System.Drawing.Point(1, 12);
+            this.PlayGround.Name = "PlayGround";
+            this.PlayGround.Size = new System.Drawing.Size(801, 376);
+            this.PlayGround.TabIndex = 0;
+            this.PlayGround.Paint += new System.Windows.Forms.PaintEventHandler(this.PlayGround_Paint);
+            this.PlayGround.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PlayGround_MouseDown);
+            // 
+            // Pause
+            // 
+            this.Pause.Image = global::WindowsFormsApplication1.Properties.Resources.Pause;
+            this.Pause.Location = new System.Drawing.Point(271, 11);
+            this.Pause.Name = "Pause";
+            this.Pause.Size = new System.Drawing.Size(256, 27);
+            this.Pause.TabIndex = 7;
+            this.Pause.TabStop = false;
+            this.Pause.Visible = false;
+            // 
+            // Continue
+            // 
+            this.Continue.Image = global::WindowsFormsApplication1.Properties.Resources.Countinue;
+            this.Continue.Location = new System.Drawing.Point(271, 162);
+            this.Continue.Name = "Continue";
+            this.Continue.Size = new System.Drawing.Size(260, 27);
+            this.Continue.TabIndex = 6;
+            this.Continue.TabStop = false;
+            this.Continue.Click += new System.EventHandler(this.Continue_Click);
+            this.Continue.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Continue_MouseDown);
+            // 
+            // Exit
+            // 
+            this.Exit.Image = global::WindowsFormsApplication1.Properties.Resources.Exit;
+            this.Exit.Location = new System.Drawing.Point(271, 217);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(260, 28);
+            this.Exit.TabIndex = 0;
+            this.Exit.TabStop = false;
+            this.Exit.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Exit_MouseDown_1);
+            // 
+            // NewGame
+            // 
+            this.NewGame.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.New_game;
+            this.NewGame.Location = new System.Drawing.Point(271, 112);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(260, 26);
+            this.NewGame.TabIndex = 5;
+            this.NewGame.TabStop = false;
+            this.NewGame.Click += new System.EventHandler(this.NewGame_Click_1);
+            this.NewGame.MouseDown += new System.Windows.Forms.MouseEventHandler(this.NewGame_MouseDown);
+            this.NewGame.MouseEnter += new System.EventHandler(this.NewGame_MouseEnter);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ball.Location = new System.Drawing.Point(379, 162);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(19, 20);
+            this.ball.TabIndex = 4;
+            this.ball.TabStop = false;
+            this.ball.Click += new System.EventHandler(this.ball_Click);
+            // 
+            // Bot
+            // 
+            this.Bot.Location = new System.Drawing.Point(0, 370);
+            this.Bot.Name = "Bot";
+            this.Bot.Size = new System.Drawing.Size(798, 10);
+            this.Bot.TabIndex = 3;
+            this.Bot.TabStop = false;
+            // 
+            // Top
+            // 
+            this.Top.Location = new System.Drawing.Point(0, -20);
+            this.Top.Name = "Top";
+            this.Top.Size = new System.Drawing.Size(798, 10);
+            this.Top.TabIndex = 2;
+            this.Top.TabStop = false;
+            // 
+            // Player2
+            // 
+            this.Player2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Player2.Location = new System.Drawing.Point(3, 135);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(14, 91);
+            this.Player2.TabIndex = 1;
+            this.Player2.TabStop = false;
+            // 
+            // Player1
+            // 
+            this.Player1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Player1.Location = new System.Drawing.Point(784, 135);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(14, 91);
+            this.Player1.TabIndex = 0;
+            this.Player1.TabStop = false;
+            this.Player1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans Ultra Bold Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(275, -2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Player 1: 0      Player 2: 0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.AllowFullOpen = false;
+            // 
+            // Bonus
+            // 
+            this.Bonus.BackColor = System.Drawing.Color.Crimson;
+            this.Bonus.Location = new System.Drawing.Point(365, -12);
+            this.Bonus.Name = "Bonus";
+            this.Bonus.Size = new System.Drawing.Size(18, 20);
+            this.Bonus.TabIndex = 6;
+            this.Bonus.TabStop = false;
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 387);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PlayGround);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Game";
+            this.Text = "Ping Pong";
+            this.Load += new System.EventHandler(this.Game_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.PlayGround.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Pause)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Continue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Exit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NewGame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Top)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Bonus)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PlayGround;
+        private System.Windows.Forms.PictureBox Player2;
+        private System.Windows.Forms.PictureBox Player1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox Top;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Bot;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox NewGame;
+        private System.Windows.Forms.PictureBox Exit;
+        private System.Windows.Forms.PictureBox Continue;
+        private System.Windows.Forms.PictureBox Pause;
+        private System.Windows.Forms.PictureBox Bonus;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Game : Form
+    {
+        
+        bool esc = false;   //Is Escape is pressed
+        int speed = 6;     //Speed of the players
+        float speedNow = 0.2f;   //Ball speed per second
+        int win = 0;    //Who win        
+        int p1Wins = 0;
+        bool noSuprise = true;
+        int p2Wins = 0;
+        bool p1Down;
+        float ballUpSpeed = 2f;
+        float ballLeftSpeed = 2f;
+        bool p2Down;
+        bool p1Up;
+        bool p2Up;
+
+        
+
+        public Game()
+        {
+            InitializeComponent();
+            // Cursor.Hide();
+            // FormBorderStyle = FormBorderStyle.None;
+            ball.BorderStyle = BorderStyle.None;
+            //TopMost = true;
+
+            NewGame.Visible = true;
+            Exit.Visible = true;
+            Player1.Visible = false;
+            Player2.Visible = false;
+            ball.Visible = false;
+            label1.Visible = false;
+            timer1.Interval = 9999999;
+            //this.Bounds = Screen.PrimaryScreen.Bounds;
+            //ball.Top = PlayGround.Bottom - (PlayGround.Bottom / 10);
+            Bonus.Visible = false;
+
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+          
+
+
+            timer1.Interval = 1;
+            if (p1Up)
+            {
+                if (Player1.Top >= Top.Top + 20)
+                {
+                    Player1.Top -= speed;
+                }
+            }
+
+            if (p2Up)
+            {
+                if (Player2.Top >= Top.Top + 20)
+                {
+                    Player2.Top -= speed;
+                }
+            }
+            if (p1Down)
+            {
+                if (Player1.Bottom <= Bot.Bottom - 15)
+                {
+                    Player1.Top += speed;
+                }
+            }
+            if (p2Down)
+            {
+                if (Player2.Bottom <= Bot.Bottom - 15)
+                {
+                    Player2.Top += speed;
+                }
+            }
+            if (ball.Top <= Top.Top + 25)
+            {
+                ballUpSpeed = -2f - speedNow ;
+                ball.BackColor = Color.Cyan;
+            }
+            if (ball.Bottom >= Bot.Bottom - 15)
+            {
+                ballUpSpeed = 2f +speedNow;
+                ball.BackColor = Color.Red;
+            }
+            if (Player2.Location.X + 15 >= ball.Location.X && ball.Location.Y >= Player2.Location.Y && ball.Location.Y <= Player2.Location.Y + 71)
+            {
+                ballLeftSpeed = -2f - speedNow;
+                speedNow += 0.3f;
+                ball.BackColor = Color.GreenYellow;
+            }
+            if (Player1.Location.X - 15 <= ball.Location.X && ball.Location.Y >= Player1.Location.Y && ball.Location.Y <= Player1.Location.Y + 71)
+            {
+                ballLeftSpeed = 2f + speedNow;
+                ball.BackColor = Color.Gold;
+                speedNow += 0.3f;
+            }
+           //Dynamic Move
+           // if (Player2.Location.X + 15 >= ball.Location.X && ball.Location.Y >= Player2.Location.Y && ball.Location.Y <= Player2.Location.Y + 20)
+           // {
+           //     ballLeftSpeed = -2f ;
+           //     ballUpSpeed += speedNow;
+           //     speedNow += 0.3f;
+           //     ball.BackColor = Color.GreenYellow;
+           // }
+
+            if (Player2.Location.X >= ball.Location.X + 10)
+            {
+                win = 1;
+            }
+            if (Player1.Location.X <= ball.Location.X - 10)
+            {
+                win = 2;
+            }
+            ball.Top -= (int)ballUpSpeed;
+            ball.Left -= (int)ballLeftSpeed;
+            if (win > 0)
+            {
+
+                speedNow = 0.2f;
+                ballLeftSpeed = 2f + speedNow; 
+                timer1.Interval = 1000;
+                if (win == 1)
+                {
+                    p1Wins++;
+                }
+                if (win == 2)
+                {
+                    p2Wins++;
+                }
+                label1.Text = "Player 1: " + p1Wins + "      Player 2: " + p2Wins;
+                ball.Top = PlayGround.Bottom / 2;
+                ball.Left = PlayGround.Right / 2;
+                win = 0;
+        
+                if (p2Wins == 3)
+                {
+
+                    timer1.Interval = 999999;
+                    label1.Visible = false;
+                    NewGame.Visible = true;
+                    Player1.Visible = false;
+                    Player2.Visible = false;
+                    ball.Visible = false;
+                    Exit.Visible = true;
+                    speedNow = 0.2f;
+                }
+                if (p1Wins == 3)
+                {
+
+                    label1.Visible = false;
+                    NewGame.Visible = true;
+                    Player1.Visible = false;
+                    Player2.Visible = false;
+                    ball.Visible = false;
+                    Exit.Visible = true;
+                    timer1.Interval = 999999;
+                    speedNow = 0.2f;
+                }
+
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down)
+            {
+                p1Down = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                p1Up = true;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                p2Down = true;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                p2Up = true;
+            }
+            if (e.KeyCode == Keys.Escape && esc == false)
+            {
+
+                label1.Visible = false;
+                NewGame.Visible = true;
+                Player1.Visible = false;
+                Player2.Visible = false;
+                ball.Visible = false;
+                Exit.Visible = true;
+                Continue.Visible = true;
+                timer1.Interval = 999999;
+                Pause.Visible = true;
+                esc = true;
+
+
+
+
+            }
+            else
+            {
+                label1.Visible = true;
+                NewGame.Visible = false;
+                Player1.Visible = true;
+                Player2.Visible = true;
+                ball.Visible = true;
+                Exit.Visible = false;
+                Continue.Visible = false;
+                timer1.Interval = 1;
+                Pause.Visible = false;
+                esc = false;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)
+            {
+                p1Up = false;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                p2Up = false;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                p2Down = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                p1Down = false;
+            }
+        }
+
+        private void ball_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PlayGround_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NewGame_Click(object sender, EventArgs e)
+        {
+            
+
+
+        }
+
+        private void NewGame_MouseEnter(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void NewGame_MouseDown(object sender, MouseEventArgs e)
+        {
+            p1Wins = 0;
+            p2Wins = 0;
+            NewGame.Visible = false;
+            label1.Visible = true;
+            Continue.Visible = false;
+            Player1.Visible = true;
+            Player2.Visible = true;
+            ball.Visible = true;
+            label1.Text = "Player 1: " + p1Wins + "      Player 2: " + p2Wins;
+            ball.Top = PlayGround.Bottom / 2;
+            ball.Left = PlayGround.Right / 2;
+            timer1.Interval = 1;
+            speedNow = 0.2f;
+            Exit.Visible = false;
+            ballLeftSpeed = 2f + speedNow;
+            Pause.Visible = false;
+        }
+
+       
+
+        private void PlayGround_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Exit_MouseDown_1(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void NewGame_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Continue_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Continue_MouseDown(object sender, MouseEventArgs e)
+        {
+                label1.Visible = true;
+                NewGame.Visible = false;
+                Player1.Visible = true;
+                Player2.Visible = true;
+                ball.Visible = true;
+                Exit.Visible = false;
+                Continue.Visible = false;
+                timer1.Interval = 1;
+                Pause.Visible = false;
+               
+        }
+
+        
+    }
+}
